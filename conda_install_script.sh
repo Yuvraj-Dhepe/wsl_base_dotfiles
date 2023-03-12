27255cd6cf58c2f1519b516d792c9b83f7945445
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2023-03-12 17:51:19 +0100

    Formatted conda_install_script V1.1

--- conda_install_script.sh
+++ conda_install_script.sh
@@ -4,7 +4,7 @@
 
 
 #if [ "$cn" = "y" ] || [ "$cn" = "Y" ]; then
-	
+
 # Function to check if a command is installed
 command_exists () {
 	command -v "$1" &> /dev/null
@@ -37,7 +37,7 @@ if ! command_exists conda; then
 		echo "-------------------------------------------"
 		wget -P ~/downloads/setups https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/downloads/setups/Miniconda3-X86_64.sh
 	fi
-	
+
 	echo "-------------------------------------------"
 	echo "Installing Conda...(set the install location to ~/downloads/installed/miniconda3)"
 	echo "-------------------------------------------"
@@ -49,63 +49,60 @@ if ! command_exists conda; then
 	echo "-------------------------------------------"
 	eval "$($HOME/downloads/installed/miniconda3/bin/conda shell.zsh hook)"
 	conda init
-	
+
 	#echo "Initializing the conda environment, Refreshing the omz shell, .zshrc file and activating the base environment."	
 	#echo "-------------------------------------------"
 	#echo "OMZ reload"
 	#echo "-------------------------------------------"
-	
+
 	echo "-------------------------------------------"
 	echo "Conda Installed successfully with version: "
 	echo "-------------------------------------------"
 	conda --version
-	
+
 	echo "-------------------------------------------"
 	echo "Conda Env's "
 	echo "-------------------------------------------"
 	conda env list
-	
+
 	# nohup bash $HOME/side_projects/omz_reload.sh > /dev/null 2>&1 &
 	#zsh $HOME/.zshrc
-	
+
 	#bash $HOME/side_projects/omz_reload.sh 
 	#> /dev/null 2>&1 &
 	# The nohup command is used to run the script in the background and the > /dev/null 2>&1 & redirects all the output to the null device and detaches the script from the terminal so that your current script execution doesn't stop.
-	
-		
+
 	echo "-------------------------------------------"
 	echo "Install an env with following command"
 	echo "sh $HOME/.dotfiles/conda_env_create.sh"
 	echo "-------------------------------------------"
-	
-	
-	
+
+
+
 	zsh $HOME/.zshrc
 	$(which zsh) -i -c "omz reload"
-	
 
-			
+
 	# echo "-------------------------------------------"
 	# echo "Activating the base environment."
 	# echo "-------------------------------------------"
 	# conda activate base
 
-	
+
 	# echo "-------------------------------------------"
 	# echo "Refresh .zshrc file"
-	# echo "-------------------------------------------"	
+	# echo "-------------------------------------------"
 	# zsh $HOME/.zshrc
-	
-	
+
 	# # # This can also be used instead of conda init.
 	# # #echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.zshrc
-	
+
 else
 	echo "-------------------------------------------"
 	echo "conda already installed"
 	echo "-------------------------------------------"
 	conda --version
-	
+
 	echo "-------------------------------------------"
 	echo "Install an env with following command"
 	echo "sh $HOME/.dotfiles/conda_env_create.sh"
